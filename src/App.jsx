cd58eb1e2ff7888bf7608a83622ddc2a5881f9aa
maryam adn <maryamahmaddahlan@gmail.com>
2022-08-26 14:46:23 +0800

    checking api for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [track, setTrack] = useState('')
 
   const api_key = 'YjZhOGJkYzYtMmY3Zi00ZjgxLTg4NmUtYWZmNDljY2UzZjcy'
   const type = '&type=track' //diff categories of search gives diff. if not specified(dropdown) just get track?/all?
@@ -9,13 +11,13 @@ function App() {
 
   fetch(`https://api.napster.com/v2.2/search?apikey=${api_key}&query=${input}${type}`)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => setTrack(data.search.data.tracks[0].previewurl));
 
   return (
     <div className="App">
       <h1>music.</h1>
       <audio
-      src='https://listen.hs.llnwd.net/g3/prvw/3/0/7/8/2/2351128703.mp3'
+      src={track}
       autoPlay
       controls
       />
